2ce6212f0890cf1e5ea13409f529f90527a2a8c2
Alegreman <alegreman09@gmail.com>
2018-09-11 17:45:07 +0000

    mas formas de escribir arreglos

--- arreglo_01.rb
+++ arreglo_01.rb
@@ -1,1 +1,10 @@
-array = ["uno",]
+array = ["uno","dos","tres"]
+
+array.each do |valor|
+  puts valor
+  end
+ array.each do |valor|
+  puts "el valor del arreglo es: #{valor}"
+  end
+  
+  array.each {|valor| puts "el valor del arreglo es: #{valor}"}
