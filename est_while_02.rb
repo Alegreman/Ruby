9b938baede023390d800cb431eed0b54fb63335b
Alegreman <alegreman09@gmail.com>
2018-09-13 17:34:18 +0000

    programas que imprimen los pares de un arreglo de n longitud en diferentes maneras

--- est_while_02.rb
+++ est_while_02.rb
@@ -3,3 +3,6 @@ while line = gets
 end
 #para procesar un archivo escribir en la consola 
 #ruby est_while_02.rb < poema.txt
+#ruby nombreprograga < archivotxt
+#para escribir una entrada del teclado y convertirla a mayuscula
+#se usa ruby nombredeprograma enter
