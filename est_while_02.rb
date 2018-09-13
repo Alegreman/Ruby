bdc7cdf8bd6cb89c26962222d726236a7665b0b9
Alegreman <alegreman09@gmail.com>
2018-09-13 17:19:49 +0000

    Programa que procesa cada linea de un archivo de texto y la convierte a mayusculas

---
+++ est_while_02.rb
@@ -0,0 +1,5 @@
+while line = gets
+ puts line.upcase
+end
+#para procesar un archivo escribir en la consola 
+#ruby est_while_02.rb < poema.txt
---
+++ imprime_par.rb
@@ -0,0 +1,11 @@
+def par(num)
+ while num.odd?
+  puts "el numero #{num} es par"
+end
+def impar(num)
+ while num.even?
+ puts "el numero #{num} es impar"
+end
+par(3)
+impar(3)
+
